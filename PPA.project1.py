2d881ecdbfcb4b8339c1fdfe1d692bd7e76defea
1oneeleven <lgnd_247@icloud.com>
2025-01-01 23:47:38 +0000

    initial code for my window app
    imported tkinter, initialise window, set size

--- PPA.project1.py
+++ PPA.project1.py
@@ -6,8 +6,28 @@ Created on Fri Nov 29 16:48:27 2024
 @author: alialshehhi
 """
 
+'''
 print ("hello world")
 
 print ("this is my 2nd commit")
 
 print ("this is my 3rd test to commit and push my code")
+'''
+
+
+#the following code is my FC723 portflio task 1
+
+
+#import tkinter with an alias
+import tkinter as GUI
+
+window = GUI.TK ()  #TK () method to initialise a main window
+window.title ("winter cafe")  # set title
+window.geometry ("400x800")
+
+
+
+window.mainloop()
+
+
+
