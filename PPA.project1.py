1c3e8185854684841e5aefb3cc40f2ad22389b63
1oneeleven <lgnd_247@icloud.com>
2025-01-04 05:22:51 +0000

    slight changes in variables + ttk import
    returned the tkinter alias to tk to avoid  confusion. + imported  ttk from tkinter  for more enhanced widgets for my GUI

--- PPA.project1.py
+++ PPA.project1.py
@@ -10,27 +10,31 @@ Created on Fri Nov 29 16:48:27 2024
 
 
 #import tkinter with an alias
-import tkinter as gui
+import tkinter as tk
+from tkinter import ttk
 
-window = gui.Tk ()  #TK () method to initialise a main window
-window.title ("winter cafe")  # set title
+
+window = tk.Tk ()  #TK () method to initialise a main window
+window.title ("Winter Kaf")  # set title
 window.geometry ("414x896") #set similar dimensions/resolutions as ios apple iphone 15 pro max
-
 """
 window.geometry ("1440x900") #set similar dimensions/resolutions as my apple macbook pro 13 inch.
 """
+#craet widget with label title, set a welcome message and slogan for my winter cafe with font and font size
+#Pack a label of the title with a couple of arguments.
+title_label = ttk.Label (master = window, text = "Welcome to Winter Kaf!", font = ("Times, 20 bold")) 
+title_label.pack() 
 
-#set a welcome message and slogan for my winter cafe with font and font size
-label = gui.Label (window, text = "Welcome to Winter Cafe!", font = ("Times", 18 ))
-label.pack() 
-label = gui.Label (window, text = "Not all our coffees are cold this winter ;)", font =("times"))
+label = tk.Label (window, text = "Not all our coffees are cold this winter ;)", font =("times"))
 label.pack()
+
+#---------
 
 def eat_in_screen ():
     label.config (text="please choose your order for eating in")
     window.title("In Cafe Menu")
 
-eatin_button = gui.Button (window, text = "Eat In", command = eat_in_screen)
+eatin_button = tk.Button (window, text = "Eat In", command = eat_in_screen)
 eatin_button.pack()
 
 def take_out_screen():
@@ -38,7 +42,7 @@ def take_out_screen():
     window.title("Take Out Menu")
 
     
-takeout_button = gui.Button (window, text = "Take Out", command = take_out_screen)
+takeout_button = tk.Button (window, text = "Take Out", command = take_out_screen)
 takeout_button.pack()
 
 
